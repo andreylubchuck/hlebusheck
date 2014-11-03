9728d9f6bcbbe77795c13fd04a7206ada0fe2fbd
Alexandr <snegovoy@hotmail.com>
2014-11-03 19:40:57 +0200

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <iostream>
-#include "exceptions.h"
+//#include "exceptions.h"
 
 class Base
 {
-
+private:
+  char* name;
+public:
+  Base(): name("") { }
+  Base(const char* name) { this->name = name; }
+  ~Base()
+  {
+    delete name;
+    name = 0;
+  }
 
 }
